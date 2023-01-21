453c3de605181966e22767ccb50a9f552a078eba
rahatalmas <rahat.almas5599@gmail.com>
2023-01-22 03:43:26 +0600

    os-node

---
+++ practice/node.js
@@ -0,0 +1,12 @@
+const os = require('os');
+
+console.log(os.EOL)
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.freemem())
+console.log(os.homedir())
+console.log(os.getPriority())
+console.log(os.hostname())
+console.log(os.networkInterfaces())
+console.log(os.userInfo())
+console.log(os.constants)
